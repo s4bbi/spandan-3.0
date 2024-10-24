53139ca6e5c5da3122531c928a42443224903922
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-10-24 22:44:31 +0530

    Coming Soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import soon from "../src/assets/soon.jpeg";
+import "./index.css"
 
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <img src={soon} alt="soon img" className="w-screen h-screen object-fill" />
-      <p>ABC</p>
+      <img src={soon} alt="soon img" className="w-screen h-screen object-cover" />
     </div>
   );
 }
