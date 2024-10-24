ffb4fb4ae02fe1adfd7f7753f6614c531a09f90c
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-10-24 23:07:01 +0530

    mock images added for display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import soon from "../src/assets/soon.jpeg";
+import soon_phone from "../src/assets/soon_phone.webp";
 import "./index.css"
 
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <img src={soon} alt="soon img" className="w-screen h-screen object-cover" />
+      <img src={soon} alt="soon img" className="w-screen h-screen object -cover hidden md:flex" />
+      <img src={soon_phone} alt="phone img" className="w-screen h-screen object-cover flex md:hidden"/>
     </div>
   );
 }
