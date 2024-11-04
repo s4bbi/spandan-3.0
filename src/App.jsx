f5ca35ae050371e88dddc721e8e24d78529a1920
Yashpreet Singh <singhyashpreet22@gmail.com>
2024-11-04 12:03:24 +0530

    Revert "Add footer file"
    This reverts commit 4ea302eba0aa1095b0f66cd9920b71cfe9ff60e2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import Footer from "../src/components/Footer/Footer";
-// import soon from "../src/assets/soon.webp";
-// import soon_phone from "../src/assets/soon_phone.webp";
+import soon from "../src/assets/soon.webp";
+import soon_phone from "../src/assets/soon_phone.webp";
 import "./index.css"
 
 const App = () => {
   return (
-    
-    <div className="h-screen w-screen min-h-screen flex flex-col justify-center items-center">
-      {/* <img src={soon} alt="soon img" className="w-screen h-screen object -cover hidden md:flex" /> */}
-      {/* <img src={soon_phone} alt="phone img" className="w-screen h-screen object-cover flex md:hidden"/> */}
-      <Footer />
+    <div className="h-screen w-screen flex justify-center items-center">
+      <img src={soon} alt="soon img" className="w-screen h-screen object -cover hidden md:flex" />
+      <img src={soon_phone} alt="phone img" className="w-screen h-screen object-cover flex md:hidden"/>
     </div>
   );
 }
