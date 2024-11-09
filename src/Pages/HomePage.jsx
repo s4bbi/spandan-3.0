a293a3a89dd9f6cd0d595a2b7a6ebde23170644c
Shivam Dangwal <shivamdangwal04@gmail.com>
2024-11-09 19:16:17 +0530

    Edit HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,33 +6,36 @@ import CountdownTimer from '../components/CountdownTimer/CountdownTimer';
 import Days from '../components/HomeComponents/Days';
 import Partners from '../components/HomeComponents/Partners';
 import Casousel from '../components/HomeComponents/Carousel';
-import More from '../components/More/More'
+import More from '../components/More/More';
 import "../index.css";
 
 const App = () => {
   const targetDate = new Date("2024-11-18T09:59:59");
+
   return (
-     <div className="h-screen w-screen flex flex-col bg-black mt-0">
+    <div className="h-screen w-screen flex flex-col bg-black mt-0">
       <Navbar />
-      <div className=" flex items-left  justify-center  mt-0"> {/* Adjust margin-top here */}
-        <HeroSection /> 
+      <div className="w-full flex items-left justify-center mt-0">
+        <HeroSection />
       </div>
-      <CountdownTimer targetDate={targetDate} />
-      <div>
-      <Days />
+      <div className="w-full">
+        <CountdownTimer targetDate={targetDate} />
       </div>
-      <div>
-        <More/>
+      <div className="w-full">
+        <Days />
       </div>
-      <div>
-      <Partners />
+      <div className="w-full">
+        <More />
       </div>
-      <div className='bg-black'>
-      <Casousel />
+      <div className="w-full bg-black">
+        <Casousel />
+      </div>
+      <div className="w-full">
+        <Partners />
       </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
